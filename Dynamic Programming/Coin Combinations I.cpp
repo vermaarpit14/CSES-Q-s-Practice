432519e896987082ef73ebe8a631f45254d144f7
vermaarpit14 <vermaarpit253@gmail.com>
2025-07-09 00:45:17 +0530

    Solved 3 Qs

---
+++ Dynamic Programming/Coin Combinations I.cpp
@@ -0,0 +1,62 @@
+/************************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define per(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define each(x, a) for (auto &x : a)
+
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+
+vector<bool> sieve(int n)
+{
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= n; ++i)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+                isPrime[j] = false;
+        }
+    }
+    return isPrime;
+}
+/************************************************************************************/
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // code here
+    int n, x;
+    cin >> n >> x;
+    vector<int> c(n), dp(x + 1, 0);
+    rep(i, 0, n) cin >> c[i];
+    sort(all(c));
+    dp[0] = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i - c[j] >= 0)
+            {
+                dp[i] = ((dp[i])%MOD + (dp[i-c[j]]%MOD))%MOD;
+            } else {
+                break;
+            }
+        }
+    }
+    cout << dp[x] << "\n";
+    return 0;
+}
